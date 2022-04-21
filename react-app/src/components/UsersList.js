5f12d0695ae8b33f5dbfdf23ae91e92a6019b71e
Nick Austin <nickaustin@Nicks-Mac-mini.local>
2022-04-20 21:51:00 -0600

    deploy fix

--- react-app/src/components/UsersList.js
+++ react-app/src/components/UsersList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector} from 'react-redux';
 import { NavLink, useHistory } from 'react-router-dom';
 
 function UsersList() {
