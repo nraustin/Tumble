3f66621a1436aa2430174b11f02f8de5cc37f602
nraustin <nickademic@gmail.com>
2023-02-28 14:53:07 -0700

    revert to original table names

--- app/models/matchedUsers.py
+++ app/models/matchedUsers.py
@@ -3,7 +3,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 matched_Users = db.Table('matched_Users', 
                     db.Column("matchedRoom_id",
                             db.Integer, 
-                            db.ForeignKey(add_prefix_for_prod("matched_rooms.id")),
+                            db.ForeignKey(add_prefix_for_prod("matchedRooms.id")),
                             primary_key=True),
                     db.Column("user_id",
                             db.Integer,
